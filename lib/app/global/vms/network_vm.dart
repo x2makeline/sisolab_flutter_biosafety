28199ad66d791bcf9e89067a93fc42768cbc8241
acasbng <acasbng@unitsoft.kr>
2023-09-05 17:44:25 +0900

    work 구조 분석중

--- lib/app/global/vms/network_vm.dart
+++ lib/app/global/vms/network_vm.dart
@@ -4,6 +4,9 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
 import 'package:sisolab_flutter_biosafety/core/utils/mc_logger.dart';
 
+/**
+ * 영구 유지 -
+ */
 class NetworkVm extends GetxService with PLoggerMixin {
   final _connectivity = Connectivity();
 
